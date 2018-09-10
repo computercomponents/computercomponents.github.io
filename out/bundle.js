1375c7dcf0d1b07cbcf43de1a8df6e1b87297f99
Evgeny T <official.trusov.mail@gmail.com>
2018-09-10 22:53:37 +0700

    Added more data into index page

--- out/bundle.js
+++ out/bundle.js
@@ -172,7 +172,18 @@ eval("function webpackEmptyContext(req) {\n\tvar e = new Error(\"Cannot find mod
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("var map = {\n\t\"./demo.jpg\": \"./indexPage/modules/news/img/demo.jpg\"\n};\n\n\nfunction webpackContext(req) {\n\tvar id = webpackContextResolve(req);\n\treturn __webpack_require__(id);\n}\nfunction webpackContextResolve(req) {\n\tvar id = map[req];\n\tif(!(id + 1)) { // check for number or string\n\t\tvar e = new Error(\"Cannot find module '\" + req + \"'\");\n\t\te.code = 'MODULE_NOT_FOUND';\n\t\tthrow e;\n\t}\n\treturn id;\n}\nwebpackContext.keys = function webpackContextKeys() {\n\treturn Object.keys(map);\n};\nwebpackContext.resolve = webpackContextResolve;\nmodule.exports = webpackContext;\nwebpackContext.id = \"./indexPage/modules/news/img sync recursive \\\\.(?:png|jpe?g|gif|tiff)$/i\";\n\n//# sourceURL=webpack:///./indexPage/modules/news/img_sync_\\.(?");
+eval("var map = {\n\t\"./cpu.jpg\": \"./indexPage/modules/news/img/cpu.jpg\",\n\t\"./demo.jpg\": \"./indexPage/modules/news/img/demo.jpg\"\n};\n\n\nfunction webpackContext(req) {\n\tvar id = webpackContextResolve(req);\n\treturn __webpack_require__(id);\n}\nfunction webpackContextResolve(req) {\n\tvar id = map[req];\n\tif(!(id + 1)) { // check for number or string\n\t\tvar e = new Error(\"Cannot find module '\" + req + \"'\");\n\t\te.code = 'MODULE_NOT_FOUND';\n\t\tthrow e;\n\t}\n\treturn id;\n}\nwebpackContext.keys = function webpackContextKeys() {\n\treturn Object.keys(map);\n};\nwebpackContext.resolve = webpackContextResolve;\nmodule.exports = webpackContext;\nwebpackContext.id = \"./indexPage/modules/news/img sync recursive \\\\.(?:png|jpe?g|gif|tiff)$/i\";\n\n//# sourceURL=webpack:///./indexPage/modules/news/img_sync_\\.(?");
+
+/***/ }),
+
+/***/ "./indexPage/modules/news/img/cpu.jpg":
+/*!********************************************!*\
+  !*** ./indexPage/modules/news/img/cpu.jpg ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"\\\\news\\\\img\\\\cpu.jpg\";\n\n//# sourceURL=webpack:///./indexPage/modules/news/img/cpu.jpg?");
 
 /***/ }),
 
